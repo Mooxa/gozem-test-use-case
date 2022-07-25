935c142ca417473b5179ec1d8a2bce8d1d46cf6c
Mouhamed Dieye <dieyem@gmail.com>
2022-07-25 17:46:38 +0000

    Initial Commit

---
+++ Location/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  Location
+//
+//  Created by macbook on 25/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
